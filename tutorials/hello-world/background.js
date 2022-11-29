5b518aa4b7013b2633c2445200615acd3e39f35b
huseyin.emecen <huseyin.emecen@dia.com.tr>
2022-11-29 17:59:22 +0300

    first

--- tutorials/hello-world/background.js
+++ tutorials/hello-world/background.js
@@ -23,20 +23,28 @@ function showReadme() {
 
       // Get the image file
       var c1 = doc.querySelector('body > div.rw_v8.search_v8 > p:nth-child(1) > a');
-      var pAll = doc.querySelectorAll('#APL li');
+      var pAll = doc.querySelectorAll('#APL');
       // document.querySelector("#datas").innerHTML = c1;
       // var pTitle = doc.querySelectorAll('#APL li a');
       // var pPrice = doc.querySelector("#APL > li:nth-child(2) > div > ul > li.b").querySelector("a span span").innerText
-      console.log(c1);
+      var prod= [];
+      console.log(pAll)
       for (let p of pAll) {
-        //console.log(p.innerText)
-        var pTitle = p.querySelector('a').attributes.title.value;
-        // var pPriceAll = p.querySelector("div.p_w_v8");
+        // console.log(p.querySelector('*'))
+        var pRow = p.querySelectorAll('li');
+        // var pCols = p.querySelector('li div');
+        var pTitle = p.querySelector('a.iC').attributes.title.value;
+        var pPriceAll = p.querySelector("span figure").outerHTML;
         var pPrice = p.querySelector("span > span > span.pb_v8 > span").innerText;
+        prod.push(pTitle,pPriceAll,pPrice)
         // console.log(pPriceAll);
-        console.log(pPrice, pTitle);
+        // console.log(pPrice, pTitle);
+        // console.log(pRow);
+        // console.log(pCols);
 
       }
+      // console.log(prod);
+
     }).catch(function (err) {
       // There was an error
       console.warn('Something went wrong.', err);
@@ -55,5 +63,6 @@ function showReadme() {
 
   // console.log(x)
   fpc(searchProd)
+
 }
 
